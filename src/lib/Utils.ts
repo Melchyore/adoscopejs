361a4871c50fb93dcc6b605c5fe89ffbf829cb3b
Paradoxe <rider-2000@hotmail.com>
2019-05-25 02:54:22 +0200

    feat(utils): add new methods for parsing exceptions and logs watchers

--- src/lib/Utils.ts
+++ src/lib/Utils.ts
@@ -8,6 +8,8 @@
 
 import * as _ from 'lodash'
 
+import BaseException from '../Exceptions/BaseException'
+
 /**
  * Collection of useful functions.
  *
@@ -40,6 +42,42 @@ export default abstract class Utils {
     const lowerCasedValue = value.toLowerCase()
 
     return _.includes(['true', 'false'], lowerCasedValue) ? lowerCasedValue === 'true' : value
+  }
+
+  static parseStringException (exception: string): BaseException {
+    if (exception) {
+      exception = exception.replace(/(?:\(node:\d+\)+\s(\[.+\]\s)?)?/giu, '')
+                            .replace('UnhandledPromiseRejectionWarning:', '')
+                            .trim()
+
+      const lines = exception.split('\n')
+      const error = lines[0].replace(/\(rejection id: \d+\)+/u, '')
+      const pattern = /(?<name>[a-zA-Z0-9_]+){1}(?::\s(?<code>[A-Z0-9_]+)?)?:\s(?<message>.+)/u
+      const { groups: { name, code, message } } = pattern.exec(error)
+
+      if (name) {
+        const exceptionClass = new BaseException(message, code)
+        exceptionClass.name = name
+        exceptionClass.stack = lines.join('\n')
+
+        return exceptionClass
+      }
+    }
+  }
+
+  static stripANSIColor (string: string): object | string {
+    const pattern = /\x1B[[(?);]{0,2}(;?\d)*./g
+
+    if (pattern.exec(string)) {
+      const strippedString = string.replace(pattern, '').trim().split(': ')
+
+      return {
+        level: strippedString[0],
+        text: strippedString[1]
+      }
+    }
+
+    return string
   }
 
 }
